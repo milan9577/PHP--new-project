0875f52f9ab7a708e419ac06557c9c0e5d213bc9
Milan Ray <100866485+milan9577@users.noreply.github.com>
2025-05-03 12:00:52 +0200

    Add files via upload

---
+++ main.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>project</title>
+</head>
+<body>
+    
+
+        <div class="logo-1">
+            <img src="img/logo.jpg" alt="IMG">
+
+            <div class="text-1">
+                <h1>Mcode.com</h1>
+            </div>
+        </div>
+
+        <div class="topnav">
+          <a class="active" href="main.php">Home</a>
+          <a href="show.php">Data show</a>
+          </div>
+
+
+
+          <div class="container">
+            <form action="">
+              <div class="row">
+                <div class="col-25">
+                  <label for="fname">Student-name</label>
+                </div>
+                <div class="col-75">
+                  <input type="text" id="fname" name="firstname" placeholder="Student-name..">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-25">
+                  <label for="lname">Student-id</label>
+                </div>
+                <div class="col-75">
+                  <input type="text" id="lname" name="lastname" placeholder="Student-id..">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-25">
+                  <label for="lname">Student-roll</label>
+                </div>
+                <div class="col-75">
+                  <input type="text" id="lname" name="lastname" placeholder="Student-roll..">
+                </div>
+              </div>
+              <div class="btn">
+                <input type="submit" value="Submit">
+              </div>
+
+            </form>
+          </div>
+
+</body>
+</html>
---
+++ show.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>project</title>
+</head>
+<body>
+    
+
+        <div class="logo-1">
+            <img src="img/logo.jpg" alt="IMG">
+
+            <div class="text-1">
+                <h1>Mcode.com</h1>
+            </div>
+        </div>
+
+        <div class="topnav">
+            <a class="active" href="main.php">Home</a>
+            <a href="show.php">Data show</a>
+          </div>
+
+
+
+          <div class="show-result">
+            <div class="container">
+                <table>
+                    <tr>
+                      <th><b>ID</b></th>
+                      <th><b>NAME</b></th>
+                      <th><b>ROLL</b></th>
+                      <th><b>EDIT</b></th>
+                      <th><b>DELETE</b></th>
+                    </tr>
+                    <tr>
+                      <td>01</td>
+                      <td>milan</td>
+                      <td>05</td>
+                      <td><button class="show-btn">edit</button></td>
+                      <td><button class="show-btn">delete</button></td>
+                    </tr>
+                  </table> 
+              </div>
+          </div>
+</body>
+</html>
